a27504320870988b9b8ffee4fba17a8f3d3b25f5
thohuynh1512 <thohuynh1512@gmail.com>
2019-05-24 20:11:55 +0700

    completed

---
+++ Nhan_Tay.cpp
@@ -0,0 +1,22 @@
+﻿/*
+Lập trình in ra màn hình mô phỏng phép nhân tay 2 số nguyên dương có
+3 chữ số nhập từ bàn phím bang tay*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Nhap 2 so ";
+	unsigned int a, b, hangTram, hangChuc, hangDV, kqDV, kqChuc, kqTram, kq;
+	cin >> a >> b;
+	hangTram = b / 100;
+	hangChuc = (b - hangTram * 100) / 10;
+	hangDV = b - hangTram * 100 - hangChuc * 10;
+	kqDV = a * hangDV;
+	kqChuc = a * hangChuc;
+	kqTram = a * hangTram;
+	kq = kqTram * 100 + kqChuc * 10 + kqDV;
+	cout << kq;
+}
+
